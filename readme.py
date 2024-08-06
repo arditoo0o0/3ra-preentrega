0ed924024e1a1a36c039775024e31618baa28c18
arditoo0o0 <ignacio.ardito.coder@gmail.com>
2024-08-06 18:54:02 -0300

    cambios

---
+++ readme.py
@@ -0,0 +1,5 @@
+#Se crea una pagina web segun las pautas del proyecto donde se desarrolla una WEB Django con patron MVT# 
+#subida a Github el mismo incluye herencia de HTML-Por lo menos 3 clases en models - Un formulario para buscar#
+#algo en la BD - archivo readme que indique el orden que se prueban las cosas y/o donde estan las funciones#
+#es un archivo orientado a una pagina educativa donde profesores y estudiantes se puedan registrar y tengan
+#distintas funcionalidades , tambien anotarse a distintos cursos , etc#
